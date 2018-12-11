ed19a47d30b032da42b0ba97f4ce7fb25ea11748
jlintvedt <jlintvedt@gmail.com>
2018-12-11 12:08:13 +0100

    Add Day04 stubs

---
+++ AdventOfCode/Day04.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Day04
+    {
+        public static int Puzzle1(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Puzzle2(string[] input)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day04Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode;
+using AdventOfCodeTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests
+{
+    [TestClass()]
+    public class Day04Tests
+    {
+        [TestMethod()]
+        public void Puzzle1ExampleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Puzzle1Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Puzzle2ExampleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Puzzle2Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
