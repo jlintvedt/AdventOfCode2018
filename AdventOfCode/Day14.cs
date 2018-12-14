f3097aab334ac91dc6f1f0981d1df3d5fe9acd10
Jonas Lintvedt <jlintvedt@gmail.com>
2018-12-14 11:32:10 +0100

    Solve Day14 Puzzle1

---
+++ AdventOfCode/Day14.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Day14
+    {
+        public class Elf
+        {
+            public int Recipe;
+            public int Pos;
+
+            public Elf(int startRecipe, int startPos)
+            {
+                Recipe = startRecipe;
+                Pos = startPos;
+            }
+        }
+
+        public class Cookbook
+        {
+            private int[] recipes;
+            private int numRecipes;
+            private int trainingRecipes;
+            private Elf[] elfs;
+            
+            public Cookbook(int[] startRecipes, int numTrainingRecipes)
+            {
+                numRecipes = startRecipes.Length;
+                if (numRecipes != 2) {
+                    throw new ArgumentException("Only works with exactly 2 starting elfs");
+                }
+                trainingRecipes = numTrainingRecipes;
+                // Need room for all training recipes + the 10 goal recipes + 1 potential overflow
+                recipes = new int[numTrainingRecipes + 10 + 1];
+                elfs = new Elf[numRecipes];
+                for (int i = 0; i < numRecipes; i++){
+                    recipes[i] = startRecipes[i];
+                    elfs[i] = new Elf(recipes[i], i);
+                }
+            }
+
+            public string FindIdealRecipes()
+            {
+                while (numRecipes < trainingRecipes+10)
+                {
+                    MakeHotChocolate();
+                }
+                var idealRecipes = recipes.Skip(trainingRecipes).Take(10);
+                return string.Join("",idealRecipes);
+            }
+
+            private void MakeHotChocolate()
+            {
+                // Find new recipe
+                var newRecipe = 0;
+                foreach (var elf in elfs){
+                    newRecipe += elf.Recipe;
+                }
+                // Add to cookbook
+                if (newRecipe >= 10)
+                {
+                    recipes[numRecipes++] = newRecipe / 10;
+                    // Should be safe as long as we only add two values <10, with a potential max of 18
+                    recipes[numRecipes++] = newRecipe - 10;
+                } else{
+                    recipes[numRecipes++] = newRecipe;
+                }
+                // Move elfs
+                foreach (var elf in elfs){
+                    elf.Pos = (elf.Pos + elf.Recipe + 1) % numRecipes;
+                    elf.Recipe = recipes[elf.Pos];
+                }
+            }
+        }
+
+
+        public static string Puzzle1(int recipesToMake)
+        {
+            var cb = new Cookbook(new int[] { 3, 7 }, recipesToMake);
+            return cb.FindIdealRecipes();
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day14Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests
+{
+    [TestClass()]
+    public class Day14Tests
+    {
+        [TestMethod()]
+        public void Puzzle1ExampleTest()
+        {
+            Assert.AreEqual("5158916779", Day14.Puzzle1(9));
+            Assert.AreEqual("0124515891", Day14.Puzzle1(5));
+            Assert.AreEqual("9251071085", Day14.Puzzle1(18));
+            Assert.AreEqual("5941429882", Day14.Puzzle1(2018));
+        }
+
+        [TestMethod()]
+        public void Puzzle1Test()
+        {
+            Assert.AreEqual("5992684592", Day14.Puzzle1(165061));
+        }
+
+    }
+}
