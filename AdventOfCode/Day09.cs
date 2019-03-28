e444d109be9e130923964f2c60e6fb6eb8c178c8
jlintvedt <jlintvedt@gmail.com>
2019-03-28 11:00:51 +0100

    Solve Day09 Puzzle1

---
+++ AdventOfCode/Day09.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Day09
+    {
+        public class MarbleGame
+        {
+            public List<int> Marbles;
+            private int activeMarblePos = 0;
+            private int numPlayers;
+            private int[] playerScores;
+
+            public MarbleGame(int numPlayers)
+            {
+                Marbles = new List<int>() { 0 };
+                this.numPlayers = numPlayers;
+                playerScores = new int[numPlayers];
+            }
+
+            public int PlayGame(int lastMarble)
+            {
+                for (int currentMarble = 1; currentMarble <= lastMarble; currentMarble++)
+                {
+                    NextMove(currentMarble);
+                }
+
+                return playerScores.Max();
+            }
+
+            private void NextMove(int marbleValue)
+            {
+                // Special rules and scoring applies if marble is multiple of 23
+                if (marbleValue%23 == 0)
+                {
+                    activeMarblePos = activeMarblePos<7 ? activeMarblePos+Marbles.Count-7 : (activeMarblePos - 7) % Marbles.Count;
+                    // Give the active player points for marble + bonus marble
+                    playerScores[marbleValue % numPlayers]+= marbleValue + Marbles[activeMarblePos];
+                    Marbles.RemoveAt(activeMarblePos);
+                    // Need to re-check active marble in case the last marble was removed
+                    activeMarblePos %= Marbles.Count;
+                    return;
+                }
+                // Normal move by adding marble to circle
+                activeMarblePos = (activeMarblePos + 2) % Marbles.Count;
+                Marbles.Insert(activeMarblePos, marbleValue);
+            }
+        }
+
+        public static int Puzzle1(int numPlayers, int lastMarble)
+        {
+            var mg = new MarbleGame(numPlayers);
+            return mg.PlayGame(lastMarble);
+        }
+
+        public static int Puzzle2(string s)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day09Tests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using AdventOfCodeTests;
+
+namespace AdventOfCode.Tests
+{
+    [TestClass]
+    public class Day09Tests
+    {
+        //string inputDay09;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //inputDay09 = InputData.ResourceManager.GetObject("D09_Puzzle").ToString();
+        }
+
+        // == == == == == Puzzle 1 == == == == ==
+        
+        [TestMethod]
+        public void Puzzle1ExampleTest()
+        {
+            Assert.AreEqual(32, Day09.Puzzle1(9, 25));
+            Assert.AreEqual(8317, Day09.Puzzle1(10, 1618));
+            Assert.AreEqual(146373, Day09.Puzzle1(13, 7999));
+            Assert.AreEqual(2764, Day09.Puzzle1(17, 1104));
+            Assert.AreEqual(54718, Day09.Puzzle1(21, 6111));
+            Assert.AreEqual(37305, Day09.Puzzle1(30, 5807));
+        }
+
+        // Runtime ...
+        [TestMethod]
+        public void Puzzle1Test()
+        {
+            Assert.AreEqual(374287, Day09.Puzzle1(468, 71010));
+        }
+
+        // == == == == == Puzzle 2 == == == == ==
+        [TestMethod]
+        public void Puzzle2ExampleTest()
+        {
+            Assert.AreEqual(3, Day09.Puzzle2(""));
+        }
+
+        // Runtime ...
+        [TestMethod]
+        public void Puzzle2Test()
+        {
+            Assert.AreEqual(1023, Day09.Puzzle2(""));
+        }
+    }
+}
